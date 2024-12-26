c62e342dd197557b4ab0e6669558bfeae3c410f5
The Coder 🧑🏼‍💻 <115723431+the-coder-o@users.noreply.github.com>
2024-12-26 11:32:42 +0500

    Uzc2 2 (#3)
    * fix page routing
    
    * fix page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Toaster } from 'sonner'
-import { useScrollToTop } from '@/hooks/useScrollTop'
 import { AuthProvider } from '@/auth/context'
 import { Router } from '@/routes'
 import { Header } from '@/sections/header/header.tsx'
 import { Footer } from '@/sections/footer/footer.tsx'
 
 const App = () => {
-  useScrollToTop()
-
   return (
     <AuthProvider>
       <Header />
       <Toaster />
-      <Router />
       <Footer />
+      <Router />
     </AuthProvider>
   )
 }
